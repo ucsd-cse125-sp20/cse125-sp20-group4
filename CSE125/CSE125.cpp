477014a8960f4d09c8557483a29b904bacfa8616
Patrick <ptliu@ucsd.edu>
2020-04-14 11:29:03 -0700

    Added logger

--- CSE125/CSE125.cpp
+++ CSE125/CSE125.cpp
@@ -1,5 +1,6 @@
 #include<GL/glut.h>
 
+
 void display() {
 
     glClear(GL_COLOR_BUFFER_BIT);
@@ -32,7 +33,7 @@ void myinit() {
 
 
 void main(int argc, char** argv) {
-
+    
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
