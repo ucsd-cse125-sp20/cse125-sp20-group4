9aafe52867f8775b4b6e25ea0e5e59f1c74a1f6d
ThiagoTGM <thiagotgmgaming@gmail.com>
2020-04-15 16:05:34 -0700

    Restructured project into CMake multiproject.

---
+++ Client/src/Client.cpp
@@ -0,0 +1,15 @@
+﻿// Client.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+#include "spdlog/spdlog.h"
+
+using namespace std;
+
+int main()
+{
+	spdlog::info( "Logging time!" );
+
+	cout << "Hello CMake again." << endl;
+	return 0;
+}
