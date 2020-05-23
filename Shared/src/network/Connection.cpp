ab927ecbd54ae7c4d51ced30a4c33001288f98ff
Thiago Marback <tmarback@ucsd.edu>
2020-05-23 01:41:48 -0700

    Implemented connection wrapper.

--- Shared/src/network/Connection.cpp
+++ Shared/src/network/Connection.cpp
@@ -3,10 +3,12 @@
 
 #include "logger.h"
 #include "network/Connection.h"
+#include "network/ConnectionClosedException.h"
 
-constexpr int BUFLEN = 512;
-constexpr unsigned int MAX_ERRORS = 5;
+/* Size of the receive buffer */
+constexpr int BUFLEN = BUFSIZ;
 
+/* Character used to mark the start of a special expression */
 constexpr char MARKER_CHAR = '&';
 #define SEQUENCE( s ) ( MARKER_CHAR + std::string( s ) + ';' )
 
@@ -36,10 +38,11 @@ void Connection::send( const std::string & message ) {
 
     std::string buf;
     insertMessage( message, buf );
-    int sent = std::send( sock, buf.c_str(), buf.size(), 0 );
+    int sent = ::send( sock, buf.c_str(), ( int ) buf.size(), 0 );
     if ( sent == SOCKET_ERROR ) {
         int error = WSAGetLastError();
         LOGGER->error( "Failed to send data: {}", error );
+        throw std::runtime_error("Failed to send data.");
     } else if ( sent < buf.size() ) {
         LOGGER->warn( "Only sent {} out of {} bytes!", sent, buf.size() );
     }
@@ -52,34 +55,21 @@ std::string Connection::receive() {
     unsigned int errorCount = 0;
     do {
 
-        char inbuf[DEFAULT_BUFLEN];
-        int received = server->recv( inbuf, BUFLEN );
+        char inbuf[BUFLEN];
+        int received = ::recv( sock, inbuf, BUFLEN, 0 );
         if ( received == SOCKET_ERROR ) { // Error
             int error = WSAGetLastError();
             spdlog::error( "Failed to receive data: {}", error );
-            if ( errorCount++ == MAX_ERRORS ) {
-                throw std::runtime_error( "Exceeded maximum receive errors." );
-            }
+            throw std::runtime_error( "Failed to receive data." );
         } else if ( received == 0 ) { // Connection closed
-            // TODO
+            throw ConnectionClosedException();
         } else {
             std::string in_str( inbuf, received );
-            spdlog::trace( "Received message from server: {0}", in_str ); //only for testing, can be removed
-
-            // todo push game state onto event queue
-            std::string message = leftover + in_str;
-            spdlog::trace( "Full message: {0}", message ); //only for testing, can be removed
-            leftover = deserializer.deserializeUpdates( message, updates );
-            spdlog::trace( "Message leftover: {}", leftover );
-            if ( !updates->empty() ) {
-                //@Thiago this pointer to unordered map gives you ID to object mappings, and objects give you position/direction TODO
-                spdlog::debug( "Number of updates: {}", updates->size() );
-                Window::world->handleUpdates( updates );
-                updates->clear();
-            }
+            inboundBuffer += in_str;
         }
 
     } while ( !extractMessage( inboundBuffer, message ) );
+    return message;
 
 }
 
@@ -94,6 +84,12 @@ std::string Connection::sanitize( const std::string & message ) {
 std::string Connection::restore( const std::string & sanitized ) {
 
     return std::regex_replace( sanitized, MARKER_SEQ_REGEX, MARKER );
+
+}
+
+void  Connection::insertMessage( const std::string & message, std::string & buffer ) {
+
+    buffer += BEGIN_SEQ + sanitize( message ) + END_SEQ;
 
 }
 
@@ -134,11 +130,5 @@ bool  Connection::extractMessage( std::string & buffer, std::string & message ) 
     message = restore( buffer.substr( startPos, endPos - startPos ) );
     buffer = buffer.substr( endPos + END_SEQ.size() ); // Keep trailing content
     return true;
-
-}
-
-void  Connection::insertMessage( const std::string & message, std::string & buffer ) {
-
-    out += BEGIN_SEQ + sanitize( message ) + END_SEQ;
 
 }
