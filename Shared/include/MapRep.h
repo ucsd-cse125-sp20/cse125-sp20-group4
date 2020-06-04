ec0f90e2063cb0ad980c0c1ea6f4e6f07a2f23e5
Tony Lau <ual_ynot@yahoo.com>
2020-06-03 19:53:31 -0700

    Fixed up get path function and refactored some unclear names

--- Shared/include/MapRep.h
+++ Shared/include/MapRep.h
@@ -8,36 +8,34 @@
 
 #include "ObjectClasses/objects.h"
 
+#define MAX_DIM 1024
 
 class MapRep {
-
-	struct xy_coord {
+private:
+	struct MapCoord {
 		int x, z;
 	};
 	struct block {
 		std::shared_ptr<Object> obj = nullptr;
-		int value;
+		int pathCost;
 		block();
 		block(std::shared_ptr<Object>obj, int val);
 	};
+	block map[MAX_DIM][MAX_DIM];
+	int height, width;
 public:
 	
 	void addObject(std::shared_ptr<Object> object, glm::vec3 pos);
 	void removeObject(glm::vec3 pos);
 	bool containsObject(glm::vec3 pos);
-	std::shared_ptr<Object> getObjectAtCoord(xy_coord pos);
+	std::shared_ptr<Object> getObjectAtCoord(MapCoord pos);
 	std::shared_ptr<Object> getObjectAtPos(glm::vec3 pos);
 
-	std::vector<glm::vec3> getPath(glm::vec3 start, glm::vec3 end);
+	std::list<glm::vec3> getPath(glm::vec3 start, glm::vec3 end);
 	MapRep(int height = 100, int width = 100);
 
-	xy_coord mapPosToCoord(glm::vec3 pos);
-	glm::vec3 coordToMapPos(xy_coord pos);
+	MapCoord mapPosToCoord(glm::vec3 pos);
+	glm::vec3 coordToMapPos(MapCoord pos);
 	void GridifyMapPos(glm::vec3& pos);
-	bool coordInBounds(xy_coord coord);
-
-private:
-	block map[200][200];
-	int height, width;
-
+	bool coordInBounds(MapCoord coord);
 };
