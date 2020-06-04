894c23c0390bb93fda65664d67cc8f97ccbc2ca9
Chris_Desktop <cmaytom@ucsd.edu>
2020-06-04 12:43:01 -0700

    can pickup barricades

--- Shared/src/EventClasses/GameState/pickup.cpp
+++ Shared/src/EventClasses/GameState/pickup.cpp
@@ -14,6 +14,7 @@ void PickUpEvent::apply(GameState* gamestate) const
     glm::vec3 dir = glm::vec3(object->getOrientationX(), 0.0f, object->getOrientationZ());
 
     std::shared_ptr<Shelf> itemObject = std::dynamic_pointer_cast<Shelf>(gamestate->map->getObjectAtPos(object->getPosition() + dir*1.5f));
+    std::shared_ptr<Barricade> barricade = std::dynamic_pointer_cast<Barricade>(gamestate->map->getObjectAtPos(object->getPosition() + dir * 1.5f));
 
     // check if found
     if (object != nullptr && itemObject != nullptr && object->getHeldItem()==nullptr) {
@@ -34,6 +35,16 @@ void PickUpEvent::apply(GameState* gamestate) const
             }
             gamestate->setDirty(true);
             log->debug("Just picked up an item {}",item->serialize());
+        }
+    } if (barricade != nullptr && object->getHeldItem() == nullptr) {
+        log->trace("passed first check trying to get item from {}", barricade->getId());
+        if (glm::distance(object->getPosition(), barricade->getPosition()) < 2.0f) {
+            log->warn("passed second check");
+            auto bar = std::make_shared<Barricade>();
+            bar->setDurability(barricade->getDurability());
+            bar->setUp(barricade->isUp());
+            object->setHeldItem(bar);
+            gamestate->deleteObject(barricade->getId());
         }
     } else {
         glm::vec3 tmp = object->getPosition() + dir * 1.5f;
